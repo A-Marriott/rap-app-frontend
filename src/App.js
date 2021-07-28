750f0d4c45a77f7d757c1a83fe11356cba2c1740
Alex Marriott <marriottaog@gmail.com>
2021-07-28 18:02:53 +0100

    WIP serving random video to youtube iframe

--- src/App.js
+++ src/App.js
@@ -2,11 +2,7 @@ import { useState, useEffect } from "react";
 import './App.css';
 
 function App() {
-  const [videos, setVideos] = useState([
-    { title: 'Car insurance', price: 267.13},
-    { title: 'MOT', price: 132.16},
-    { title: 'Exhaust', price: 50.31}
-  ]);
+  const [videos, setVideos] = useState(null);
 
   useEffect(() => {
     getVideos();
@@ -22,9 +18,21 @@ function App() {
       .then((response) => response.json())
       .then((data) => setVideos(data))
 
+  let randomVideo = '5bDxoiUTn4w'
+
+  if (videos !== null) {
+    randomVideo = videos[Math.floor(Math.random() * videos.length)]
+  }
+  // if (videos === null) {
+  // } else {
+  //   let
+  // }
 
   return (
     <div className="App">
+      <iframe id="player" width="640" height="390"
+      src={`https://www.youtube.com/embed/${randomVideo}?autoplay=1&loop=1`}>
+      </iframe>
       {videos?.map((video, i) => (
         <p>{video.genre}</p>
       ))}
